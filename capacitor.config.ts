9ba5666c20edb8fadf9f1265f3301435886b4537
isackson99 <is.serrano@duocuc.cl>
2024-10-03 22:44:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'smartmanager_frontend_ionic',
+  webDir: 'www'
+};
+
+export default config;
